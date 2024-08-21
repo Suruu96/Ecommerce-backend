20c0cd004a9eec82922bc75ea3b2106bd4825d37
Suruu96 <118453796+Suruu96@users.noreply.github.com>
2024-08-21 22:54:18 +0530

    Initial commit with backend code

---
+++ controller/emailCtrl.js
@@ -0,0 +1,24 @@
+const asyncHandler = require('express-async-handler');
+
+const sendEmail = require("./emailCtrl");
+
+const forgotPasswordToken = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User not found with this email");
+  try {
+    const token = await user.createPasswordResetToken();
+    await user.save();
+    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+    const data = {
+      to: email,
+      text: "Hey User",
+      subject: "Forgot Password Link",
+      htm: resetURL,
+    };
+    sendEmail(data);
+    res.json(token);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
